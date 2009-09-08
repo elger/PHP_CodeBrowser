744ee606ca79eafc2c70b419224061ea192e3d45
Elger Thiele <elger.thiele@mayflower.de>
2009-09-08 14:22:15 +0000

    phpdoc

--- CodeBrowser.php
+++ CodeBrowser.php
@@ -1,25 +1,72 @@
 <?php
+/**
+ * PHP_CodeBrowser
+ *
+ * Copyright (c) 2007-2009, Mayflower GmbH
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *   * Neither the name of Mayflower GmbH nor the names of his
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @category   PHP_CodeBrowser
+ * @package    PHP_CodeBrowser
+ * @author     Elger Thiele <elger.thiele@mayflower.de>
+ * @copyright  2007-2009 Mayflower GmbH
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    SVN: $Id$
+ * @link       http://www.phpunit.de/
+ * @since      File available since 1.0
+ */
+
+define('PHPCB_INSTALL_DIR', q);
+
+require_once PHPCB_INSTALL_DIR . "/src/Util/Autoloader.php";
 
 /**
  * Start File
  * PHP file called from the command line with arguments
  * 
  * Arguments:
- * --source	Projekts PHP Source folder
+ * --source Projekts PHP Source folder
  * --output HTML Output folder 
  * --xml    Cruise Control XML Source File
  *
- * @package     CodeBrowser
- * @version     CVS: $Id: CodeBrowser.php,v 1.2 2008/02/26 11:06:05 christopher Exp $
- * @author      Christopher Weckerle <christopher.weckerle@mayflower.de>
- * @copyright   2007-2008 Mayflower GmbH
+ * @category   Testing
+ * @package    PHPUnit
+ * @author     Elger Thiele <elger.thiele@mayflower.de>
+ * @author     Christopher Weckerle <christopher.weckerle@mayflower.de>
+ * @copyright  2007-2009 Mayflower GmbH
+ * @license    http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @version    Release: @package_version@
+ * @link       http://www.phpunit.de/
+ * @since      Class available since 1.0
  */
-
-// Installation directory
-define('PHPCB_INSTALL_DIR', dirname(__FILE__));
-
-// required files
-require_once PHPCB_INSTALL_DIR . "/src/Util/Autoloader.php";
 
 // register autoloader
 spl_autoload_register(array(
