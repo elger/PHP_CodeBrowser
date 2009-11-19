bcb451db95f7bc8f2865b8ca98081e99c6913113
Elger Thiele <elger.thiele@mayflower.de>
2009-11-19 08:20:13 +0000

    Adapted install path for pear package

--- bin/phpcb.php
+++ bin/phpcb.php
@@ -50,7 +50,7 @@
 if (strpos( '@php_dir@', '@php_dir' ) === 0) {
     require_once dirname( __FILE__ ) . '/../src/CLIController.php';
 } else {
-    require_once '@php_dir@/PHP_CodeBrowser/src/CLIController.php';
+    require_once '@php_dir@/PHP_CodeBrowser/CLIController.php';
 }
 
 cbCLIController::main();
