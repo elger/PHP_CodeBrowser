f2da16d2cbbcb44b359fa69fdd530a9b004802c7
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2010-06-13 17:32:36 +0200

    Added a sidebar in which the filetree is shown

--- templates/js/tree.js
+++ templates/js/tree.js
@@ -2,4 +2,8 @@ $(function() {
     $("#tree").jstree({
         "plugins" : ["html_data", "themes"]
     });
+
+    var w = $("#tree").width() + 40;
+    var h = $("#tree").height() + 40;
+    $("#tree").sidebar({width: w, height: h, open: "click", close: "click", position:"left"});
 });
