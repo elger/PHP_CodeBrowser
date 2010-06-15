9c265da08469e44d606bc9e3cb984ba7c0289533
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2010-06-15 17:13:39 +0200

    Changed layout. Tree doesn't use the sidebar anymore.

--- templates/js/tree.js
+++ templates/js/tree.js
@@ -5,5 +5,4 @@ $(function() {
 
     var w = $("#tree").width() + 40;
     var h = $("#tree").height() + 40;
-    $("#tree").sidebar({width: w, height: h, open: "click", close: "click", position:"left"});
 });
