378d407dd729d9753230812962f5eb42addbfbfe
Simon Kohlmeyer <simon.kohlmeyer@googlemail.com>
2010-06-22 12:06:49 +0200

    Renamed CbIssueHandler to CbSourceHandler.
    Additionally to handling the issues from the log, it will be handling
    the optional source directory given by --source.
    
    This will help building an interface for the sourcefiles that is
    indifferent to whether a source dir was given or not.

--- src/SourceHandler.php
+++ src/SourceHandler.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Issue handler
+ * Source handler
  *
  * PHP Version 5.3.0
  *
@@ -40,6 +40,7 @@
  * @package   PHP_CodeBrowser
  * @author    Elger Thiele <elger.thiele@mayflower.de>
  * @author    Michel Hartmann <michel.hartmann@mayflower.de>
+ * @author    Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
  * @copyright 2007-2010 Mayflower GmbH
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   SVN: $Id$
@@ -48,10 +49,9 @@
  */
 
 /**
- * CbIssueHandler
+ * CbSourceHandler
  *
- * This class is providing a lists of errors as well lists of filenames that have
- * related errors.
+ * This class manages lists of source files and their issues.
  * For providing these lists the prior generated CbIssueXml is parsed.
  *
  * @category  PHP_CodeBrowser
@@ -59,13 +59,14 @@
  * @author    Elger Thiele <elger.thiele@mayflower.de>
  * @author    Christopher Weckerle <christopher.weckerle@mayflower.de>
  * @author    Michel Hartmann <michel.hartmann@mayflower.de>
+ * @author    Simon Kohlmeyer <simon.kohlmeyer@mayflower.de>
  * @copyright 2007-2010 Mayflower GmbH
  * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
  * @version   Release: @package_version@
  * @link      http://www.phpunit.de/
  * @since     Class available since 1.0
  */
-class CbIssueHandler
+class CbSourceHandler
 {
     /**
      * CbIssueXml object
